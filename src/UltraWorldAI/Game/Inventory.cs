a5fb2409700c902bdb45ff0cf701b2396b36170e
Tiagobuzzz <64416525+Tiagobuzzz@users.noreply.github.com>
2025-06-06 06:42:09 -0300

    Add inventory query and removal methods

--- src/UltraWorldAI/Game/Inventory.cs
+++ src/UltraWorldAI/Game/Inventory.cs
@@ -9,4 +9,18 @@ public class Inventory
 
     public void Add(Item item) => _items.Add(item);
     public bool Remove(Item item) => _items.Remove(item);
+
+    public bool Contains(string itemName)
+        => _items.Exists(i => i.Name == itemName);
+
+    public bool RemoveByName(string itemName)
+    {
+        var index = _items.FindIndex(i => i.Name == itemName);
+        if (index >= 0)
+        {
+            _items.RemoveAt(index);
+            return true;
+        }
+        return false;
+    }
 }
